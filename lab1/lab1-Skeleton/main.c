133f25e720fae2bb704c0f1850f7eca59948fb08
Georgi Baghdasaryan <baghdasaryan@ucla.edu>
2013-09-30 14:51:38 -0700

    updated .gitignore to include Vim stuff and changed gcc options in the Makefile

--- lab1/lab1-Skeleton/main.c
+++ lab1/lab1-Skeleton/main.c
@@ -46,13 +46,14 @@ main (int argc, char **argv)
 
   script_name = argv[optind];
   FILE *script_stream = fopen (script_name, "r");
-  if (! script_stream)
+  if (!script_stream)
     error (1, errno, "%s: cannot open", script_name);
   command_stream_t command_stream =
     make_command_stream (get_next_byte, script_stream);
 
   command_t last_command = NULL;
   command_t command;
+
   while ((command = read_command_stream (command_stream)))
     {
       if (print_tree)
