936e79cf31d6dbb9b328477ed09df72177863851
ntgsx92 <Sixiang@SixiangMac.local>
2013-10-16 16:56:34 -0700

    Add implementation for execute simple and subshell command

--- lab1/lab1-Skeleton/execute-command.c
+++ lab1/lab1-Skeleton/execute-command.c
@@ -2,8 +2,13 @@
 
 #include "command.h"
 #include "command-internals.h"
-
+#include <stdlib.h>
 #include <error.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <core.c>
+#include <sys/wait.h>
 
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
@@ -34,8 +39,62 @@ execute_command (command_t c, bool time_travel)
 
   	case SUBSHELL_COMMAND:
 
-  	default: //Some error handling
+  	default: //Some error handling code
 
   }
   error (1, 0, "command execution not yet implemented");
+}
+
+//fork a child process to execute simple command
+void execute_simple_command(command_t cmd, bool time_travel){
+	pid_t pid;
+	int status;
+	char ** argv;
+	char * filename;
+
+	pid = fork();
+	if(pid < 0){
+		//could not fork child process
+	}
+	else if(pid == 0){
+		//inside child process
+		//
+		if(execvp(filename, argv) < 0){
+			//Fail to execute simple command
+			print_system_error();
+		}
+
+	}
+	else{
+		//TODO: Do IO here
+		//inside parent process
+		//wait for child process to finish
+		if( waitpid(pid, &status,-1) == -1){
+			print_system_error();
+		}
+	}
+	cmd->status = WIFEXITED(status);
+}
+
+void execute_pipe_command(command_t cmd, bool time_travel){
+
+}
+
+void execute_or_command(command_t cmd, bool time_travel){
+
+}
+
+void execute_sequence_command(command_t cmd, bool time_travel){
+
+}
+
+void execute_and_command(command_t cmd, bool time_travel){
+
+}
+
+void execute_subshell_command(command_t cmd, bool time_travel){
+	TODO: Do IO here
+	command_t subshell_cmd = cmd->u.subshell_command;
+	execute_command(subshell_command, time_travel);
+	cmd->status = subshell_command->status;
 }
