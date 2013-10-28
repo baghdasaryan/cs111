34aed87ebba4c489452b12b701df7d9f4eb66096
ntgsx92 <Sixiang@SixiangMac.local>
2013-10-27 17:07:55 -0700

    Add create dependecny graph function for part c

--- lab1/lab1-Skeleton/execute-command.c
+++ lab1/lab1-Skeleton/execute-command.c
@@ -3,12 +3,13 @@
 #include "command.h"
 #include "command-internals.h"
 #include "core.h"
-
+#include "parallel.h"
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdbool.h>
 
 // Get command status
 int
@@ -218,4 +219,183 @@ execute_command (command_t c,
       print_error_and_exit("Unknown command type specified.");
     }
 }
+
+//extract input/output from command_stream_t into
+//an array of words
+void 
+get_IO(bool only_o, char **comand_io, 
+    command_stream_t command, size_t *curr_size){
+    //first determine what kind of command it is
+    if((command->cmd->type) == SIMPLE_COMMAND || (command->cmd->type) == SUBSHELL_COMMAND){
+        //extract input as well
+        if(!only_o){
+          if(command->cmd->input != NULL){
+              (*curr_size)++;
+              *command_io = checked_realloc(*command_io, sizeof(char *) * (*curr_size));
+              (*command_io)[*curr_size] = command->cmd->input;
+          }
+        }
+        //extract output
+        if(command->cmd->input != NULL){
+            (*curr_size)++;
+            *command_io = checked_realloc(*command_io, sizeof(char *) * (*curr_size));
+            (*command_io)[*curr_size] = command->cmd->input;
+        }
+    }
+    else{
+      //extract IO for other type of commands
+      get_IO(only_o, command_io, command->cmd->u.command[0], curr_size);
+      get_IO(only_o, command_io, command->cmd->u.command[1], curr_size);
+    }
+
+}
+//check if any words in cmd1 appear in cmd2
+bool 
+depend(char **cmd1, char **cmd2){
+  bool is_dependent = false;
+  int i,j = 0;
+  if( cmd1 == NULL || cmd2 == NULL){
+    return false;
+  }
+  while(cmd1[i] != NULL){
+    while(cmd2[j] != NULL){
+      if(!strcmp(cmd1[i],cmd2[j])){
+        return true;
+      }
+      j++;
+    }
+    i++;
+  }
+
+  return false;
+}
+
+//insert a node to the end of queue
+void 
+insert(dependency_t head, dependency_t last, denpendency_t new_node){
+    if(head == NULL){
+      head = new_node;
+      last = head;
+    }
+    else{
+      last->next = new_node;
+      new_node->prev = last;
+      new_node->next = NULL;
+      last = new_node;
+    }
+}
+
+//delete front of the queue
+void
+dequeue(dependency_t head){
+  if(head == NULL){
+    return;
+  }
+  dependency_t temp = head;
+  (head->next)->prev = NULL;
+  head = head->next;
+  free(temp);
+}
+
+//create a new node
+void
+create_node(cmd_node_t node, command_t c){
+    node = checked_malloc(sizeof(struct cmd_node));
+    node->dep_size = 0;
+    node->dependent = NULL;
+    node->cmd = c;
+}
+
+//create a new dependency node
+void
+create_dependency_node(cmd_node_t cmd_node, dependency_t new_dep_node){
+  new_dep_node = checked_malloc(sizeof(struct dependency));
+  new_dep_node->next = NULL;
+  new_dep_node->prev = NULL;
+  new_dep_node->c_node = cmd_node;
+}
+
+//add dependent node to another node
+void
+add_dependent_node(cmd_node_t dependent_node, cmd_node_t node){
+    (node->dep_size)++;
+    node->dependent = checked_realloc(node->dependent, sizeof(cmd_node_t) * (node->dep_size));
+    (node->dependent)[dep_size-1] = dependent_node;
+}
+
+//create a dependency graph and a no-dependency graph
+void 
+create_dependency_graph(dependency_t no_dep_first, dependency_t no_dep_last,
+                             dependency_t dep_first, dependency_t dep_last, 
+                             command_stream_t c, size_t &dep_size, size_t &no_dep_size){
+    //dep_size and no_dep_size will store the size of 
+    //dep and none_dep queue respectively.
+    dep_size = 0;
+    no_dep_size = 0;
+    command_stream_t curr, iterator = c;
+    while(curr != NULL){
+      //store how many nodes one node depend on
+      size_t num_dep_nodes = 0;
+      //construct a new empty node
+      cmd_node_t node = NULL;
+      create_node(node, curr->cmd);
+      while(iterator != curr){
+        char **curr_io = NULL;
+        char **iterator_o = NULL;
+        size_t io_counter = 0;
+        size_t o_counter = 0;
+        //extract input and output of current command 
+        getIO(false, curr_io, curr, &io_counter);
+        //extract output of command before current command
+        getIO(true, iterator_o, iterator, &o_counter);
+        //create dependency graph
+        if(depend(curr_io, iterator_o)){
+            //create dependent nodes
+            cmd_note_t dep_node = NULL;
+            create_node(dep_node, iterator->cmd);
+            //add the dependent node to current node
+            add_dependent_node(dep_node, node);
+        }
+        //start to create dependency and no_dependency queue
+        dependency_t dep_node = NULL;
+        create_dependency_node(node, dep_node);
+      }
+      //if there are dependent nodes, add it to dependency queue
+      //otherwise, add it to no dependency queue
+      if(node->dependent == NULL){
+        dependency_t new_no_dep_node = NULL;
+        create_dependency_node(new_no_dep_node, node);
+        insert(no_dep_first, no_dep_last, new_no_dep_node);
+      }
+      else{
+        dependency_t new_dep_node = NULL;
+        create_dependency_node(new_dep_node, node);
+        insert(dep_first, dep_last, new_dep_node);
+      }
+    }
+}
+
+//main function for time_travel shell
+//return last command in command_stream
+command_t 
+time_travel(comand_stream_t c){
+
+    //we will have to two queues to represent nodes that have dependency on other nodes and
+    //nodes that dont have dependency on any other nodes
+    //the first and last node will always be updated in order to
+    //iterate the queue or insert new node
+    dependency *no_dep_first = NULL;
+    dependency *no_dep_last = NULL;
+    dependency *dep_first = NULL;
+    dependecny *dep_last = NULL
+    size_t dep_size;
+    size_t no_dep_size;
+    //create both dependency graph and no dependency graph
+    create_dependency_graph(no_dep_first, no_dep_last, dep_first, dep_last,
+                            c,dep_size, no_dep_size);
+
+
+}
+
+
 
