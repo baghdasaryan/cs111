021319d14413b09afba78cdfd2cc43ddb26b5f2a
Georgi Baghdasaryan <baghdasaryan@ucla.edu>
2013-11-11 22:00:40 -0800

    finished implementing read and write functions

--- ramdisk/osprd.c
+++ ramdisk/osprd.c
@@ -272,6 +272,7 @@ int osprd_ioctl(struct inode *inode, struct file *filp,
 		pid_list_t prev = NULL,
 			   curr = NULL;
 
+		/*
 		// USED FOR TESTING crypto(...)
 		// ****************************
 		printk("Data encryption started...\n");
@@ -287,7 +288,7 @@ int osprd_ioctl(struct inode *inode, struct file *filp,
 		printk("DECRYPTED: %s\n", data);
 		printk("Data encryption finished...\n");
 		// ****************************
-
+		*/
 		osp_spin_lock(&d->mutex);
 		if (d->write_lock_holder == current->pid) {
 			osp_spin_unlock(&d->mutex);
@@ -486,38 +487,161 @@ static void osprd_process_request_queue(request_queue_t *q)
 // the Linux block device interface doesn't let a block device find out
 // which file has been closed.  We need this information.
 
-static ssize_t _osprd_read(struct file *file,
-			   char *buffer,      /* The buffer to fill with data */
-			   size_t count,      /* The length of the buffer     */
-			   loff_t *offset)    /* Our offset in the file       */
+#include <linux/cdev.h>
+
+struct scull_dev {
+	struct scull_qset *data; 	/* Pointer to first quantum set */
+	int quantum; 			/* the current quantum size */
+	int qset; 			/* the current array size */
+	unsigned long size; 		/* amount of data stored here */
+	unsigned int access_key; 	/* used by sculluid and scullpriv */
+	struct semaphore sem;		/* mutual exclusion semaphore */
+	struct cdev cdev; 		/* Char device structure */
+};
+
+struct scull_qset {
+	void **data;
+	struct scull_qset *next;
+};
+
+struct scull_qset *scull_follow(struct scull_dev *dev, int n)
 {
+	struct scull_qset *qs = dev->data;
 
+	/* Allocate first qset explicitly if need be */
+	if (! qs) {
+		qs = dev->data = kmalloc(sizeof(struct scull_qset), GFP_KERNEL);
+		if (qs == NULL)
+			return NULL;  /* Never mind */
+		memset(qs, 0, sizeof(struct scull_qset));
+	}
 
-
-	// READ CODE GOES HERE
-
-
-	// returns NUM_READ_BYTES
+	/* Then follow the list */
+	while (n--) {
+		if (!qs->next) {
+			qs->next = kmalloc(sizeof(struct scull_qset), GFP_KERNEL);
+			if (qs->next == NULL)
+				return NULL;  /* Never mind */
+			memset(qs->next, 0, sizeof(struct scull_qset));
+		}
+		qs = qs->next;
+		continue;
+	}
+	return qs;
 }
 
-static ssize_t _osprd_write(struct file *file,
-			    char *buffer,      /* The buffer to fill with data */
-			    size_t count,      /* The length of the buffer     */
-			    loff_t *offset)    /* Our offset in the file       */
+static ssize_t _osprd_read(struct file *filp,
+			   char __user *buf,	/* The buffer to fill with data */
+			   size_t count,	/* The length of the buffer     */
+			   loff_t *f_pos)	/* Our offset in the file       */
 {
+	struct scull_dev *dev = filp->private_data;
+	struct scull_qset *dptr;	/* the first listitem */
+	int quantum = dev->quantum,
+	    qset = dev->qset;
+	int itemsize = quantum * qset;  /* how many bytes in the listitem */
+	int item, s_pos, q_pos, rest;
+	ssize_t retval = 0;
+
+	if (down_interruptible(&dev->sem))
+		return -ERESTARTSYS;
+	if (*f_pos >= dev->size)
+		goto out;
+	if (*f_pos + count > dev->size)
+		count = dev->size - *f_pos;
 
 
+	/* find listitem, qset index, and offset in the quantum */
+	item = (long)*f_pos / itemsize;
+	rest = (long)*f_pos % itemsize;
+	s_pos = rest / quantum;
+	q_pos = rest % quantum;
 
-	// WRITE CODE GOES HERE
+	/* follow the list up to the right position (defined elsewhere) */
+	dptr = scull_follow(dev, item);
 
+	if (dptr == NULL ||
+		 !dptr->data ||
+		 ! dptr->data[s_pos])
+		goto out; /* don't fill holes */
 
-	// returns NUM_WRITTEN_BYTES
+	/* read only up to the end of this quantum */
+	if (count > quantum - q_pos)
+		count = quantum - q_pos;
+
+	if (copy_to_user(buf, dptr->data[s_pos] + q_pos, count)) {
+		retval = -EFAULT;
+		goto out;
+	}
+
+	*f_pos += count;
+	retval = count;
+
+out:
+	up(&dev->sem);
+	return retval;
+}
+
+static ssize_t _osprd_write(struct file *filp,
+			    char __user *buf,	/* The buffer to fill with data */
+			    size_t count,	/* The length of the buffer     */
+			    loff_t *f_pos)	/* Our offset in the file       */
+{
+	struct scull_dev *dev = filp->private_data;
+	struct scull_qset *dptr;
+	int quantum = dev->quantum,
+	    qset = dev->qset;
+	int itemsize = quantum * qset;
+	int item, s_pos, q_pos, rest;
+	ssize_t retval = -ENOMEM; /* value used in "goto out" statements */
+
+	if (down_interruptible(&dev->sem))
+		return -ERESTARTSYS;
+
+	/* find listitem, qset index and offset in the quantum */
+	item = (long)*f_pos / itemsize;
+	rest = (long)*f_pos % itemsize;
+	s_pos = rest / quantum;
+	q_pos = rest % quantum;
+
+	/* follow the list up to the right position */
+	dptr = scull_follow(dev, item);
+	if (dptr == NULL)
+		goto out;
+	if (!dptr->data) {
+		dptr->data = kmalloc(qset * sizeof(char *), GFP_KERNEL);
+		if (!dptr->data)
+			goto out;
+		memset(dptr->data, 0, qset * sizeof(char *));
+	}
+	if (!dptr->data[s_pos]) {
+		dptr->data[s_pos] = kmalloc(quantum, GFP_KERNEL);
+		if (!dptr->data[s_pos])
+			goto out;
+	}
+	/* write only up to the end of this quantum */
+	if (count > quantum - q_pos)
+		count = quantum - q_pos;
+
+	if (copy_from_user(dptr->data[s_pos]+q_pos, buf, count)) {
+		retval = -EFAULT;
+		goto out;
+	}
+	*f_pos += count;
+	retval = count;
+
+	/* update the size */
+	if (dev->size < *f_pos)
+		dev->size = *f_pos;
+
+out:
+	up(&dev->sem);
+	return retval;
 }
 
 static struct file_operations osprd_blk_fops = {
-// UNCOMMENT THE FOLLOWING TWO LINES WHEN DONE
-//	.read = _osprd_read,
-//	.write = _osprd_write
+	.read = _osprd_read,
+	.write = _osprd_write
 };
 
 static int (*blkdev_release)(struct inode *, struct file *);
