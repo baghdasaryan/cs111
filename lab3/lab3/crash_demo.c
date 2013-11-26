0887b0c1781130d88921f6b5d9fb467f1875ffea
Georgi Baghdasaryan <baghdasaryan@ucla.edu>
2013-11-26 14:11:41 -0800

    added silent mode for crash_demo.c

--- lab3/lab3/crash_demo.c
+++ lab3/lab3/crash_demo.c
@@ -11,6 +11,9 @@
 void print(char *data);
 void print_and_sleep(char *data, float time);
 
+// Global variable
+static int silent = 0;
+
 
 // Main function
 int main(int argc, char *argv[])
@@ -18,13 +21,18 @@ int main(int argc, char *argv[])
 	int ret = 0;
 
 	// Test input
-	if (argc != 2) {
-		fprintf(stderr, "Usage: crash_demo NUMBER_OF_WRITES\n");
+	if (argc != 2 && argc != 3) {
+		fprintf(stderr, "Usage: crash_demo NUMBER_OF_WRITES [SILENT]\n");
 		ret = 1;
 	} else if (atoi(argv[1]) < -1) {
 		fprintf(stderr, "NUMBER_OF_WRITES has to be >= -1\n");
 		ret = 1;
+	} else if (argc == 3 && (strcmp(argv[2], "silent") != 0 && strcmp(argv[2], "SILENT") != 0)) {
+		fprintf(stderr, "Second argument, if any, has to be SILENT\n");
+		ret = 1;
 	} else {
+		if (argc == 3)
+			silent = 1;
 		ret = crash_test(atoi(argv[1]));
 	}
 
@@ -43,8 +51,10 @@ int crash_test(int num_writes)
 	    file_descriptor = 0;
 	print_and_sleep("This program tests file system robustness by crushing the latter one.\n", 1);
 
-	fprintf(stdout, "Setting up the program with nwrites_to_crash=%d\n", num_writes);
-	sleep(1);
+	if (!silent) {
+		fprintf(stdout, "Setting up the program with nwrites_to_crash=%d\n", num_writes);
+		sleep(1);
+	}
 
 	file_descriptor = open("./test/hello.txt", O_RDONLY);
 	status = ioctl(file_descriptor, CRASHER, num_writes);
@@ -67,13 +77,16 @@ int crash_test(int num_writes)
 
 void print(char *data)
 {
-	fprintf(stdout, data);
+	if (!silent)
+		fprintf(stdout, data);
 }
 
 void print_and_sleep(char *data, float time)
 {
-	fprintf(stdout, data);
-	sleep(time);
+	if (!silent) {
+		fprintf(stdout, data);
+		sleep(time);
+	}
 }
 
 /* ***************************************** */
